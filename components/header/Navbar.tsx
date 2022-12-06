e9335c2f8f3512c0d72694559f1f9f040c3a14a4
sojebsikder <sojebsikder@gmail.com>
2022-12-06 17:08:29 +0600

    updated login page

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -4,28 +4,30 @@ export default function Navbar() {
   return (
     <>
       <nav>
-        <ul className="flex justify-center navbar overflow-hidden bg-[var(--secondary-color)]">
-          <li>
-            <Link href={"/"}>Home</Link>
-          </li>
-          <li>
-            <Link href="/pricing">Pricing</Link>
-          </li>
-          <li className="dropdown">
-            <a className="dropbtn">Resources</a>
-            <div className="dropdown-content">
-              <Link href="#">Link 1</Link>
-              <Link href="#">Link 2</Link>
-              <Link href="#">Link 3</Link>
-            </div>
-          </li>
-          <li>
-            <Link href="/login">Login</Link>
-          </li>
-          <li>
-            <Link href="/register">Sign Up Free</Link>
-          </li>
-        </ul>
+        <div className="flex justify-center mt-4">
+          <ul className="navbar-container mx-auto overflow-hidden bg-[var(--secondary-color)]">
+            <li>
+              <Link href={"/"}>Home</Link>
+            </li>
+            <li>
+              <Link href="/pricing">Pricing</Link>
+            </li>
+            <li className="dropdown">
+              <a className="dropbtn">Resources</a>
+              <div className="dropdown-content">
+                <Link href="#">Link 1</Link>
+                <Link href="#">Link 2</Link>
+                <Link href="#">Link 3</Link>
+              </div>
+            </li>
+            <li>
+              <Link href="/login">Login</Link>
+            </li>
+            <li>
+              <Link href="/register">Sign Up Free</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
     </>
   );
---
+++ pages/register.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import React from "react";
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+import Meta from "../components/header/Meta";
+
+export default function Register() {
+  return (
+    <div>
+      <Meta />
+      <main className="flex justify-center h-screen">
+        <div
+          style={{
+            padding: "50px",
+            width: "50%",
+          }}
+          className="self-center"
+        >
+          <div className="text-center font-bold">Whatsapp crm</div>
+          <div className="text-center">
+            Already have an account?{" "}
+            <Link className="text-[var(--primary-color)]" href={"/login"}>
+              Sign in
+            </Link>
+          </div>
+          <div className="m-4 btn-primary">Continue with google</div>
+          <div className="m-4">OR</div>
+          <div className="m-4">
+            <input className="input" type="email" placeholder="Work Email" />
+          </div>
+
+          <div className="m-4 btn-primary">Get Started</div>
+          <div className="m-4">
+            <span>
+              By signing up you agree to our terms of service and privacy
+              policy.
+            </span>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
