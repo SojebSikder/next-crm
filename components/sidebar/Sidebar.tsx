3571840c4e5809e8ae20ac7f7a37d0ce573d8bff
sojebsikder <sojebsikder@gmail.com>
2022-12-07 19:55:46 +0600

    main

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -6,28 +6,35 @@ export default function Sidebar() {
   return (
     <div
       className="fixed top-0 left-0 h-screen w-16 flex flex-col
-    bg-white dark:bg-gray-900 shadow-lg"
+    bg-[#ededed] dark:bg-gray-900 shadow-lg"
     >
-      <SideBarIcon icon={<BsCircle size="28" />} />
-      <Divider />
-      <SideBarIcon icon={<BsMailbox size="32" />} />
-      <SideBarIcon icon={<BsPerson size="32" />} />
-      <Divider />
-      <SideBarIcon icon={<BsGearFill size="22" />} />
+      <SideBarIcon text="My new workspace" icon={<BsCircle size="28" />} />
+      <SideBarIcon text="Messages" icon={<BsMailbox size="32" />} />
+      <SideBarIcon text="Contacts" icon={<BsPerson size="32" />} />
+      {/* <Divider /> */}
+      <SideBarIcon text="Settings" icon={<BsGearFill size="22" />} />
     </div>
   );
 }
 
 const SideBarIcon = ({
   icon,
-  text = "tooltip 💡",
+  text,
 }: {
+  /**
+   * Icon element
+   */
   icon: any;
+  /**
+   * Tooltip
+   */
   text?: string;
 }) => (
   <div className="sidebar-icon group">
     {icon}
-    <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+    {text && (
+      <span className="sidebar-tooltip group-hover:scale-100">{text}</span>
+    )}
   </div>
 );
 const Divider = () => <hr className="sidebar-hr" />;
