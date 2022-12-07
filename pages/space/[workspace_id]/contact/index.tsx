30422fb62bdc1bf0a514ca52252d666117e10beb
sojebsikder <sojebsikder@gmail.com>
2022-12-07 20:13:37 +0600

    main

--- pages/space/[workspace_id]/contact/index.tsx
+++ pages/space/[workspace_id]/contact/index.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import Meta from "../../../../components/header/Meta";
+import Sidebar from "../../../../components/sidebar/Sidebar";
+import { AppConfig } from "../../../../config/app.config";
 
 export default function Contact() {
-  return <div>index</div>;
+  return (
+    <div>
+      <Meta title={`Contacts - ${AppConfig().app.name}`} />
+      <Sidebar workspace_id="1" />
+      <main className="flex justify-center h-screen">
+        <h1>Hello world contacts here</h1>
+      </main>
+    </div>
+  );
 }
