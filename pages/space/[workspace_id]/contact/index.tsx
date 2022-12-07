cbed95b801d4c5933deca4a0a8800dfdaad93abd
sojebsikder <sojebsikder@gmail.com>
2022-12-07 22:00:05 +0600

    main

--- pages/space/[workspace_id]/contact/index.tsx
+++ pages/space/[workspace_id]/contact/index.tsx
@@ -3,95 +3,102 @@ import Meta from "../../../../components/header/Meta";
 import Sidebar from "../../../../components/sidebar/Sidebar";
 import { AppConfig } from "../../../../config/app.config";
 
-export default function Contact() {
+export const getServerSideProps = (context: {
+  query: any;
+  req?: any;
+  res?: any;
+  asPath?: any;
+  pathname?: any;
+}) => {
+  const { req, query, res, asPath, pathname } = context;
+
+  const workspace_id = query.workspace_id;
+  return {
+    props: {
+      workspace_id: workspace_id,
+    },
+  };
+};
+
+export default function Contact({ workspace_id }: { workspace_id: string }) {
   return (
-    <div>
+    <div className="flex">
       <Meta title={`Contacts - ${AppConfig().app.name}`} />
-      <Sidebar workspace_id="1" />
-      <main className="flex justify-center h-screen">
-        <div>
-          <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                <tr>
-                  <th scope="col" className="py-3 px-6">
-                    Product name
-                  </th>
-                  <th scope="col" className="py-3 px-6">
-                    Color
-                  </th>
-                  <th scope="col" className="py-3 px-6">
-                    Category
-                  </th>
-                  <th scope="col" className="py-3 px-6">
-                    Price
-                  </th>
-                  <th scope="col" className="py-3 px-6">
-                    <span className="sr-only">Edit</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <th
-                    scope="row"
-                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+      <Sidebar workspace_id={workspace_id} />
+      <main className="mt-5 ml-[80px] container mx-auto flex justify-center h-screen">
+        <div className="overflow-x-scroll w-full shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="py-3 px-6">
+                  Display
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Name
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Channels
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Email
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Phone
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Country
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Language
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Conversation Status
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Conversation Assignee
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Last Message
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  Date Added
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  {/* <span className="sr-only">Action</span> */}
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                <th
+                  scope="row"
+                  className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  Apple MacBook Pro 17{'"'}
+                </th>
+                <td className="py-4 px-6">Sliver</td>
+                <td className="py-4 px-6">Laptop</td>
+                <td className="py-4 px-6">$2999</td>
+                <td className="py-4 px-6">$2999</td>
+                <td className="py-4 px-6">$2999</td>
+                <td className="py-4 px-6">$2999</td>
+                <td className="py-4 px-6">$2999</td>
+                <td className="py-4 px-6">$2999</td>
+                <td className="py-4 px-6">$2999</td>
+                <td className="py-4 px-6">$2999</td>
+
+                <td className="py-4 px-6 text-right">
+                  <a
+                    href="#"
+                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
                   >
-                    Apple MacBook Pro 17{'"'}
-                  </th>
-                  <td className="py-4 px-6">Sliver</td>
-                  <td className="py-4 px-6">Laptop</td>
-                  <td className="py-4 px-6">$2999</td>
-                  <td className="py-4 px-6 text-right">
-                    <a
-                      href="#"
-                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                    >
-                      Edit
-                    </a>
-                  </td>
-                </tr>
-                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <th
-                    scope="row"
-                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Microsoft Surface Pro
-                  </th>
-                  <td className="py-4 px-6">White</td>
-                  <td className="py-4 px-6">Laptop PC</td>
-                  <td className="py-4 px-6">$1999</td>
-                  <td className="py-4 px-6 text-right">
-                    <a
-                      href="#"
-                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                    >
-                      Edit
-                    </a>
-                  </td>
-                </tr>
-                <tr className="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600">
-                  <th
-                    scope="row"
-                    className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                  >
-                    Magic Mouse 2
-                  </th>
-                  <td className="py-4 px-6">Black</td>
-                  <td className="py-4 px-6">Accessories</td>
-                  <td className="py-4 px-6">$99</td>
-                  <td className="py-4 px-6 text-right">
-                    <a
-                      href="#"
-                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                    >
-                      Edit
-                    </a>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
+                    Edit
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </main>
     </div>
