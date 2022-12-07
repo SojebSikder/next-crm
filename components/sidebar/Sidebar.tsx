bc5ed1a503a3dbe5b4b1585b22ebc9ef1de6591c
sojebsikder <sojebsikder@gmail.com>
2022-12-07 20:08:45 +0600

    main

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,22 +1,54 @@
+import Link from "next/link";
 import React from "react";
 import { BsGearFill, BsMailbox, BsPerson, BsCircle } from "react-icons/bs";
-import { FaCircle } from "react-icons/fa";
 
-export default function Sidebar() {
+export default function Sidebar({ workspace_id }: { workspace_id: string }) {
   return (
     <div
       className="fixed top-0 left-0 h-screen w-16 flex flex-col
     bg-[#ededed] dark:bg-gray-900 shadow-lg"
     >
-      <SideBarIcon text="My new workspace" icon={<BsCircle size="28" />} />
-      <SideBarIcon text="Messages" icon={<BsMailbox size="32" />} />
-      <SideBarIcon text="Contacts" icon={<BsPerson size="32" />} />
+      <SideBarItem text="My new workspace" icon={<BsCircle size="28" />} />
+      <SideBarItem text="Messages" icon={<BsMailbox size="32" />} />
+      <SideBarItem
+        href={`/space/${workspace_id}/contact`}
+        text="Contacts"
+        icon={<BsPerson size="32" />}
+      />
       {/* <Divider /> */}
-      <SideBarIcon text="Settings" icon={<BsGearFill size="22" />} />
+      <SideBarItem text="Settings" icon={<BsGearFill size="22" />} />
     </div>
   );
 }
 
+const SideBarItem = ({
+  icon,
+  text,
+  href,
+}: {
+  /**
+   * Icon element
+   */
+  icon: any;
+  /**
+   * Tooltip
+   */
+  text?: string;
+  /**
+   * url
+   */
+  href?: string;
+}) => (
+  <>
+    {href ? (
+      <Link href={`${href}`}>
+        <SideBarIcon text={text} icon={icon} />
+      </Link>
+    ) : (
+      <SideBarIcon text={text} icon={icon} />
+    )}
+  </>
+);
 const SideBarIcon = ({
   icon,
   text,
--- pages/space/index.tsx
+++ pages/space/index.tsx
@@ -1,15 +1,11 @@
-import Link from "next/link";
 import React from "react";
-import { Alert } from "../../components/alert/Alert";
+
 import Meta from "../../components/header/Meta";
-import { Logo } from "../../components/reusable/Logo";
-import Sidebar from "../../components/sidebar/Sidebar";
 
 export default function index() {
   return (
     <div>
       <Meta />
-      <Sidebar />
       <main className="flex justify-center h-screen">
         <h1>Hello world</h1>
       </main>
