044732fb14a282d2237b0103a9e8094e88bc9113
sojebsikder <sojebsikder@gmail.com>
2022-12-07 22:09:41 +0600

    main

--- pages/space/[workspace_id]/contact/index.tsx
+++ pages/space/[workspace_id]/contact/index.tsx
@@ -26,8 +26,8 @@ export default function Contact({ workspace_id }: { workspace_id: string }) {
       <Meta title={`Contacts - ${AppConfig().app.name}`} />
       <Sidebar workspace_id={workspace_id} />
       <main className="mt-5 ml-[80px] container mx-auto flex justify-center h-screen">
-        <div className="overflow-x-scroll w-full shadow-md sm:rounded-lg">
-          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <div className="w-full shadow-md sm:rounded-lg">
+          <table className="overflow-x-scroll w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
                 <th scope="col" className="py-3 px-6">
@@ -77,16 +77,16 @@ export default function Contact({ workspace_id }: { workspace_id: string }) {
                 >
                   Apple MacBook Pro 17{'"'}
                 </th>
-                <td className="py-4 px-6">Sliver</td>
-                <td className="py-4 px-6">Laptop</td>
-                <td className="py-4 px-6">$2999</td>
-                <td className="py-4 px-6">$2999</td>
-                <td className="py-4 px-6">$2999</td>
-                <td className="py-4 px-6">$2999</td>
-                <td className="py-4 px-6">$2999</td>
-                <td className="py-4 px-6">$2999</td>
-                <td className="py-4 px-6">$2999</td>
-                <td className="py-4 px-6">$2999</td>
+                <td className="py-4 px-6">sojebsikder</td>
+                <td className="py-4 px-6">Whatsapp</td>
+                <td className="py-4 px-6">sojebsikder@gmail.com</td>
+                <td className="py-4 px-6">8801833962595</td>
+                <td className="py-4 px-6">BD</td>
+                <td className="py-4 px-6">Bangla</td>
+                <td className="py-4 px-6">Open</td>
+                <td className="py-4 px-6">N/A</td>
+                <td className="py-4 px-6">Dec 06, 2022 10:31 AM</td>
+                <td className="py-4 px-6">Dec 06, 2022 10:31 AM</td>
 
                 <td className="py-4 px-6 text-right">
                   <a
