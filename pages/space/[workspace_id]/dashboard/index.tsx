d1cb8ec5d0a7a8dc13b8567eb394d401fbcf11c2
sojebsikder <sojebsikder@gmail.com>
2022-12-08 21:06:57 +0600

    main

--- pages/space/[workspace_id]/dashboard/index.tsx
+++ pages/space/[workspace_id]/dashboard/index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
+import Meta from "../../../../components/header/Meta";
+import Sidebar from "../../../../components/sidebar/Sidebar";
 
-export default function index() {
-  return <div>Dashboard</div>;
+export default function Dashboard() {
+  return (
+    <div>
+      <Meta />
+      <Sidebar workspace_id="1" />
+      <main className="flex justify-center h-screen">
+        <h1>Hello world</h1>
+      </main>
+    </div>
+  );
 }
