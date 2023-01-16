1b6b09913a963e2376b4fd36e6cf403709d16655
sojebsikder <sojebsikder@gmail.com>
2023-01-16 20:27:00 +0600

    update popupmenu

--- components/reusable/PopupMenu.tsx
+++ components/reusable/PopupMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 export function PopupMenu({
   label,
@@ -7,48 +7,82 @@ export function PopupMenu({
   label: string;
   children?: any;
 }) {
-  const [show, setShow] = useState(false);
-  const handleToggle = () => {
-    setShow((prev) => !prev);
-  };
   return (
     <>
-      <button
-        onClick={handleToggle}
-        id="dropdownTopButton"
-        data-dropdown-toggle="dropdownTop"
-        data-dropdown-placement="top"
-        className="mr-3 mb-3 md:mb-0 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
-      >
-        {label}
-        <svg
-          className="w-4 h-4 ml-2"
-          aria-hidden="true"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
-            clipRule="evenodd"
-          ></path>
-        </svg>
-      </button>
-      <div
-        style={{ transform: "translate(0, -270px)" }}
-        id="dropdownTop"
-        className={`z-10 ${
-          show == false ? "hidden" : ""
-        } bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700`}
-      >
-        <ul
-          className="py-1 text-sm text-gray-700 dark:text-gray-200"
-          aria-labelledby="dropdownTopButton"
-        >
-          {children}
-        </ul>
+      <div className="flex">
+        <div>
+          <div className="dropdown relative">
+            <button
+              className="
+                dropdown-toggle
+                px-6
+                py-2.5
+                bg-blue-600
+                text-white
+                font-medium
+                text-xs
+                leading-tight
+            
+                rounded
+                shadow-md
+                hover:bg-blue-700 hover:shadow-lg
+                focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
+                active:bg-blue-800 active:shadow-lg active:text-white
+                transition
+                duration-150
+                ease-in-out
+                flex
+                items-center
+                whitespace-nowrap
+              "
+              type="button"
+              id="dropdownMenuButton1"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              {label}
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="caret-down"
+                className="w-2 ml-2"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 320 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"
+                ></path>
+              </svg>
+            </button>
+            <ul
+              className="
+          dropdown-menu
+          min-w-max
+          absolute
+          hidden
+          bg-white
+          text-base
+          z-50
+          float-left
+          py-2
+          list-none
+          text-left
+          rounded-lg
+          shadow-lg
+          mt-1
+          m-0
+          bg-clip-padding
+          border-none
+        "
+              aria-labelledby="dropdownMenuButton1"
+            >
+              {children}
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
@@ -67,14 +101,27 @@ export function PopupMenuItem({
     }
   };
   return (
-    <li>
-      <a
-        onClick={handleClick}
-        // href="#"
-        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-      >
-        {children}
-      </a>
-    </li>
+    <>
+      <li>
+        <a
+          onClick={handleClick}
+          className="
+              dropdown-item
+              text-sm
+              py-2
+              px-4
+              font-normal
+              block
+              w-full
+              whitespace-nowrap
+              bg-transparent
+              text-gray-700
+              hover:bg-gray-100
+            "
+        >
+          {children}
+        </a>
+      </li>
+    </>
   );
 }
